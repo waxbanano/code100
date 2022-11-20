695d5e97ffa3727a97c5195970d513e33924d06a
Scott Grobe <waxbanano@gmail.com>
2022-11-19 19:37:09 -0600

    day1

---
+++ 100/day_1.py
@@ -0,0 +1,84 @@
+#Day 1 of 100 days of Python coding
+
+#HINT - 'If you want to feel like a proper hacker, when you've finished writing your code hit `CMD + Enter` to run your code!'
+print('Hello Replit')
+
+print("Well we")
+print("just use more lines")
+print("of code")
+
+#HINT - Use the triple quote """ if you want to write a big chunk of text with gaps or line breaks.
+print("""Anything that starts with three quotes, 
+    Here's 
+        An
+         Exact
+          Bit of 
+           Text
+and ends in three quotes can span many lines and even contain symbols within without freaking anything out""")
+
+#ERORRS#
+
+# NameError
+# You'll see this error message if:
+# you get the name of a function wrong
+# you misspell it
+# get the capitalization wrong
+
+# Print("What could go wrong?")
+
+# Traceback (most recent call last):
+#   File "main.py", line 1, in <module>
+#     Print("What could go wrong?")
+# NameError: name 'Print' is not defined
+
+# Did you notice the p in print is capitalized? print is only lower case letters.
+
+
+#SyntaxError#
+#
+# You will see this message if:
+# you have the order of the symbols wrong
+# you forget () or " "
+#
+#print "Hello Again"
+#
+#   File "main.py", line 1
+#     print "Hello Again"
+#           ^
+# SyntaxError: Missing parentheses in call to 'print'. Did you mean print("Hello Again")?
+
+#FIX MY CODE - DAY 1#
+
+#BAD CODE
+# print("= MUSIC+ =")
+# print("> Songs")
+# print("> Albums")
+# print("> Artists")
+
+# FIXED CODE
+# print("= MUSIC+ =")
+# print("> Songs")
+# print("> Albums")
+# print("> Artists")
+
+#CHALLENGE - DAY1#
+
+# 1. Write your full name and today's date in separate lines of code. HINT - Don't forget "" and () when creating your print statements.
+
+# 1.
+print("Your Full Name")
+print("Today's Date")
+
+# 2.Copy this text below. See if you can do it with just the one print statement! HINT - Remember you can use triple quotes for a large block of text. """
+
+# 2.
+print("""Your Full Name \nToday's Date""")
+
+# 3. On the next line add I am feeling with an emoji of your choice to illustrate how you're feeling about the challenge. HINT - When using Unicode with Python, replace "+" with "000" from the Unicode. And then prefix the Unicode with "\". For example- U+1F913 will be used as \U0001F913. Here, "+" is replaced with "000" and "\" is prefixed with the Unicode.
+# 3.
+print("I am feeling smart \U0001F913")
+
+# 4.Add one more line to your output You can follow my progress at replit.com/@ and add in your Replit username. This will be a great way of maintaining accountability for yourself!
+
+# 4.Add one more line to your output You can follow my progress at replit.com/@ and add in your Replit username. This will be a great way of maintaining accountability for yourself!
+print("You can follow my progress at replit.com/@waxnftbanano")
